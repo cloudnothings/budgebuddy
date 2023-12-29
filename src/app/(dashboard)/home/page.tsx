ae605f3c288ddfe8afed0bf666ce90dab30a1002
Carlos Rangel <carlos@rangel.us>
2023-12-28 23:25:43 -0800

    Remove unnecessary line in DashboardPage function

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -4,7 +4,6 @@ import { getBudgetsForUser } from "@/data-layer/budgets";
 import { auth } from "@/auth";
 import NoBudgetsView from "./_components/no-budgets-homescreen";
 
-
 export default async function DashboardPage() {
   const session = await auth()
   if (!session) {
